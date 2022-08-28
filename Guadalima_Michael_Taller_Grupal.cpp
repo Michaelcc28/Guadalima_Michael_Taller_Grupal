45edfadcabb3229643e48f666867ba310ee414b2
Michaelcc28 <109257666+Michaelcc28@users.noreply.github.com>
2022-08-28 15:41:09 -0500

    Update Guadalima_Michael_Taller_Grupal.cpp

--- Guadalima_Michael_Taller_Grupal.cpp
+++ Guadalima_Michael_Taller_Grupal.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include <fstream>
-#include <conio.h>
-#include <stdio.h>
-#include <string.h>
 using namespace std;
 struct alumno
 {
@@ -13,9 +8,9 @@ struct alumno
 int menu()
 {
     int op;
-    cout << "Menu Manejo de documentos\n";
-    cout << "1. Crear documento guardar Datos\n";
-    cout << "2. Leer documento ver Datos\n";
+    cout << "Menu Manejo de Archivos\n";
+    cout << "1. Crear Archivo guardar Datos\n";
+    cout << "2. Leer Archivo ver Datos\n";
     cout << "3. Buscar un Registro:\n";
     cout << "4. Eliminar Registro:\n";
     cout << "5. Salir\n";
@@ -25,17 +20,17 @@ int menu()
 }
 void llena_alumnos(alumno al, int n)
 {
-    string documento;
+    string archivo;
     ofstream arch;
-    cout << "Ingrese el nombre del documento: ";
+    cout << "Ingrese el nombre del archivo: ";
     cin.ignore();
-    getline(cin, documento);
-    arch.open(documento.c_str(), ios::out);
+    getline(cin, archivo);
+    arch.open(archivo.c_str(), ios::out);
     int i = 0;
     for (i = 1; i <= n; i++)
     {
         al.codigo = i;
-        cout << "Nombre Alumno Numero " << i << " ";
+        cout << "Nombre Alumno No " << i << " ";
         getline(cin, al.nombre);
         cout << "Nota Promedio: ";
         cin >> al.promedio;
@@ -44,17 +39,17 @@ void llena_alumnos(alumno al, int n)
     }
     arch.close();
 }
-void leer_documento()
+void leer_archivo()
 {
-    string documento, linea;
+    string archivo, linea;
     ifstream arch;
-    cout << "Ingrese el nombre del documento: ";
+    cout << "Ingrese el nombre del archivo: ";
     cin.ignore();
-    getline(cin, documento);
-    arch.open(documento.c_str(), ios::out);
+    getline(cin, archivo);
+    arch.open(archivo.c_str(), ios::out);
     if (arch.fail())
     {
-        cout << "No encuentra el documento\n";
+        cout << "No encuentra el archivo\n";
         return;
     }
     while (!arch.eof())
@@ -64,71 +59,94 @@ void leer_documento()
     }
     arch.close();
 }
-
 void buscar()
 {
-    string documento, linea, cod;
+    string archivo, linea, nom, codigo, nombre, nota;
     ifstream arch;
-    cout << "Ingrese el nombre del documento: ";
+    int opcmenu;
+    bool bandera = false;
+    cout << "Ingrese el nombre del archivo: ";
     cin.ignore();
-    getline(cin, documento);
-    arch.open(documento.c_str(), ios::out);
-    cout << "Digite el codigo del alumno a buscar: ";
-    cin >> cod;
+    getline(cin, archivo);
+    arch.open(archivo.c_str(), ios::out);
+    cout << "Ingrese el nombre del alumno a buscar: ";
+    cin >> nom;
+        if (arch.fail())
+        {
+            cout << "No encuentra el archivo\n";
+            return;
+        }
+        while (!arch.eof())
+        {
 
-    if (arch.fail())
-    {
-        cout << "No encuentra el documento\n";
-        return;
-    }
-    while (!arch.eof())
-    {
-        getline(arch, linea);
-        if (linea.substr(0, 1) == cod)
-        {
-            cout << linea << endl;
+            arch >> codigo >> nombre >> nota;
+            if (nombre == nom)
+            {
+                bandera = true;
+                cout << codigo << "\t" << nombre << "\t" << nota << endl;
+                break;
+            }
         }
-        else
+
+        if(bandera == false)
         {
-            cout << "El registro no hallado\n";
+            cout<<"Registro no encontrado\n";
+            
         }
+    
+    else
+    {
+        cout << "\nIngreso de opcion no valido!\n";
     }
+
 }
-
-void eliminar()
+void eliminar_registro()
 {
-    string documento, linea, cod;
+    string archivo, linea, cod, codigo, nombre, nota;
     ifstream arch;
-    ofstream aux;
-    aux.open ("auxiliar.txt", ios::out);
-    cout << "Ingrese el nombre del documento: ";
+    ofstream auxiliar;
+    bool encontrado = false;
+    char *archencontrado;
+    cout << "Ingrese el nombre del archivo: ";
     cin.ignore();
-    getline(cin, documento);
-    arch.open(documento.c_str(), ios::out);
-    cout << "Digite el codigo del alumno a buscar: ";
+    getline(cin, archivo);
+    arch.open(archivo.c_str(), ios::out);
+    auxiliar.open("auxiliar.txt", ios::out);
+    archencontrado = const_cast<char *>(archivo.c_str());
+    cout << "Digite el codigo del alumno a eliminar: ";
     cin >> cod;
+
     if (arch.fail())
     {
-        cout << "No encuentra el documento\n";
+        cout << "No encuentra el archivo\n";
         return;
     }
     while (!arch.eof())
     {
-        getline(arch, linea);
-        if (linea.substr(0, 1) == cod)
+        arch >> codigo >> nombre >> nota;
+        if (codigo == cod)
         {
-            cout << "\nEl documento se elimino" << endl;
-            getch();
+            encontrado = true;
+            cout << "\n Se elimino el registro ";
+            cout << codigo << "\t" << nombre << "\t" << nota << endl;
+            codigo = "";
+            nombre = "";
+            nota = "";
+            auxiliar << codigo << "" << nombre << "" << nota;
         }
         else
-        { 
-            aux << linea << endl;
+        {
+            auxiliar << codigo << " " << nombre << " " << nota << "\n";
         }
     }
-    remove("documento");
-    rename ( "auxiliar.txt", "documento.txt" );
-
-
+    if (encontrado == false)
+    {
+        cout << "\n\nNo se encontro al alumno con el codigo: " << cod << "\n\n";
+    }
+    arch.close();
+    auxiliar.close();
+    remove(archencontrado);
+    rename("auxiliar.txt", archencontrado);
 }
 main()
 {
@@ -146,13 +164,16 @@ main()
             llena_alumnos(al, na);
             break;
         case 2:
-            leer_documento();
+            leer_archivo();
             break;
         case 3:
             buscar();
             break;
         case 4:
-            eliminar();
+            eliminar_registro();
+            break;
+        case 5:
+            cout << "Saliendo del sistema ";
             break;
         default:
             cout << "Opción no valida ";
